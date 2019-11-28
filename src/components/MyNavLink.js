75e090f5bc0ae3e7f5f088bc427337f767b0ca0c
twelve121212 <lych121212@163.com>
2019-11-28 20:13:55 +0800

    路由正确

--- src/components/MyNavLink.js
+++ src/components/MyNavLink.js
@@ -1,32 +1,46 @@
+
 import React from "react";
+
 import {NavLink} from "react-router-dom";
+
 import router from "../router"
+
 function MyNavLink() {
+
     return (
+
         <nav className="footer">
+
             {
+
                 router.config.map(v=>(
-                    v.linkName?v.children.map(value=>(
-                        <NavLink
+
+                    v.linkName?<NavLink
+
                         className={router.className}
+
                         activeClassName={router.activeClassName}
-                        key={value.path} to={value.path} exact={value.exact}>
+
+                        key={v.path} to={v.path} exact={v.exact}>
+
                             
-                                <i className={value.className}></i>
-                                <p>{value.linkName}</p > 
-                        </NavLink>
-                    )):
-                    <NavLink
-                    key={v.path} to={v.path} exact={v.exact}>
-                        
-                            <i className={v.className}></i>
-                            <p>{v.linkName}</p>
-                               
-                    </NavLink>
+
+                                <i className={v.className}></i>
+
+                                <p>{v.linkName}</p >
+
+                            
+
+                        </NavLink>:null
+
                 ))
 
             }
+
         </nav>
+
     )
+
 }
+
 export default MyNavLink
